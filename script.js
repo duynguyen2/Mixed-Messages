99b8ec1874ee9644d93d44bc76d2cb39df9a4efb
Duy Nguyen <thenman233@gmail.com>
2024-07-26 13:45:51 -0700

    test push

---
+++ script.js
@@ -0,0 +1,24 @@
+const messages = ["It'll all be fine, it's going to work out!", "Have patience, you will be rewarded soon.", "You work so much harder than you know, so keep at it!", `“Shoot for the moon. Even if you miss, you'll land among the stars.”
+― Norman Vincent Peale`, "Don't be afraid to success!", "I know you can do it!", "You're stronger than you really know."];
+
+const randomNumber = () => {
+    const randomNumber = Math.floor(Math.random() * messages.length);
+    return randomNumber;
+}
+
+/*  Displays the messages */
+const displayMessage = () => {
+    const messageToDisplay = messages[randomNumber()];
+}
+
+/* Draws the box the messages will display in. */
+const drawBox = () => {
+
+}
+
+
+/* Function to display different message */
+const button = () => {
+
+}
+
